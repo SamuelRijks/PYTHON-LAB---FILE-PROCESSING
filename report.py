f71f538096986776596d01d356d88ba94ba151e2
Samur18 <samurijks@gmail.com>
2023-03-08 14:51:06 +0100

    cambios

--- report.py
+++ report.py
@@ -10,20 +10,25 @@ config_version = None
 config = configparser.ConfigParser()
 config.read('FW_1238.conf')
 
-pdf = FPDF()
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.add_font('Arial', '', r'C:\Windows\Fonts\arial.ttf', uni=True)
 pdf.add_page()
 
-with open('texto.txt', 'r') as f:
+with open('texto.txt', 'r', encoding='utf-8') as f:
     for i, line in enumerate(f, start=1):
         try:
             if line.startswith('(title)'):
                 pdf.set_font
                 pdf.set_font("Arial", size=16)
-                 # write the line to the PDF
+                pdf.set_text_color(255, 255, 0)  # set color to yellow
+                # write the line to the PDF
                 pdf.write(5, line[7:])  # skip '(title)' prefix
-             else:
-                 # set font size to 12
+            if line.startswith('NEWPAGE'):
+                pdf.add_page()
+            else:
+                # set font size to 12
                 pdf.set_font("Arial", size=12)
+                pdf.set_text_color(64, 64, 64)  # set color to yellow
                 # write the line to the PDF
                 pdf.write(5, line)
                 # process the line
