f6ff293b97011f127419c4aef8fb7251ca8bcace
Samur18 <samurijks@gmail.com>
2023-03-08 20:48:09 +0100

    creacion tablas

--- report.py
+++ report.py
@@ -20,11 +20,28 @@ with open('texto.txt', 'r', encoding='utf-8') as f:
             if line.startswith('(title)'):
                 pdf.set_font
                 pdf.set_font("Arial", size=16)
-                pdf.set_text_color(255, 255, 0)  # set color to yellow
+                pdf.set_text_color(255, 204, 0)  # set color to yellow
                 # write the line to the PDF
                 pdf.write(5, line[7:])  # skip '(title)' prefix
-            if line.startswith('NEWPAGE'):
+            elif line.startswith('NEWPAGE'):
                 pdf.add_page()
+            elif line.startswith('TABLE'):
+                pdf.set_font("Arial", size=12)  # set font size to 12
+                pdf.set_text_color(64, 64, 64)
+                col_width = 40
+                row_height = 6
+                margin = 10
+                # Create table header
+                pdf.cell(col_width, row_height * 2, 'Header 1', border=1)
+                pdf.cell(col_width, row_height * 2, 'Header 2', border=1)
+                pdf.cell(col_width, row_height * 2, 'Header 3', border=1)
+                pdf.ln(row_height * 2)
+                # Create table rows
+                for row in range(1, 4):
+                    for col in range(1, 4):
+                        pdf.cell(col_width, row_height,
+                                 f'Row {row}, Col {col}', border=1)
+                    pdf.ln(row_height)
             else:
                 # set font size to 12
                 pdf.set_font("Arial", size=12)
